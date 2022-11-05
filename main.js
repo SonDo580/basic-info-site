38ea7937cf4bd32632309aa9f26041fdcd8e6dbe
Son Do <dominhson580@gmail.com>
2022-11-05 21:20:03 +0700

    Send about.html and contact.url based on the url

--- main.js
+++ main.js
@@ -8,6 +8,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/about.html"));
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "/contact.html"));
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
