df0640fe15d60a7a50f625bc78cd2876a2e17e6d
Son Do <dominhson580@gmail.com>
2022-11-05 21:16:41 +0700

    Create a basic express app

--- main.js
+++ main.js
@@ -1,26 +1,8 @@
-const http = require("http");
-const fs = require("fs");
+const express = require("express");
 
-http
-  .createServer(function (req, res) {
-    const displayPage = (err, data) => {
-      if (err) {
-        throw err;
-      }
+const app = express();
+const port = 8080;
 
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    };
-
-    if (req.url === "/") {
-      fs.readFile("./index.html", displayPage);
-    } else if (req.url === "/about") {
-      fs.readFile("./about.html", displayPage);
-    } else if (req.url === "/contact") {
-      fs.readFile("./contact.html", displayPage);
-    } else {
-      fs.readFile("./404.html", displayPage);
-    }
-  })
-  .listen(8080);
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
