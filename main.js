d7249b38b7b6480e06a51c3f2f4d7516b6bfa9ba
Son Do <dominhson580@gmail.com>
2022-11-05 21:18:48 +0700

    Send index.html with res.sendFile

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const path = require("path");
 const app = express();
 const port = 8080;
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
